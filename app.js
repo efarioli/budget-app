9b91a72eeef70323cfb038edd590efba213de887
Ezequiel Farioli <efarioli@gmail.com>
2018-02-23 11:52:04 +0000

    Read data from html input - create a DOMstring Obj  to html strings var

--- app.js
+++ app.js
@@ -4,15 +4,41 @@ var budgetController = (function() {
 })();
 
 var UIController = (function(){
-    // Some code
 
-});
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInpunt: function() {
+            return{
+            type: document.querySelector(DOMstrings.inputType).value,// will be either inc or exp
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+        };
+    },
+
+    getDOMStrings: function() {
+        return DOMstrings;
+    }
+   
+    };
+
+})();
 
 var controller = (function(budgetCtrl, UICtrl) {
     //controller is the only one module that can interact with the others modules
 
+    var DOM = UICtrl.getDOMStrings();
+
     var ctrlAddItem =function() {
         // 1. Get the field input data
+
+        var input = UICtrl.getInpunt();
+        console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -23,14 +49,11 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 5. Display the budget on the UI
     };
     
-    document.querySelector('.add__btn').addEventListener('click', function() {
-        console.log('ENTER was pressed');
-      
-    });
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
         if (event.keyCode === 13 || event.which === 13){
-            console.log('ENTER was pressed');
+            ctrlAddItem();
         }
     });
 
