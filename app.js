005d269e47f9470c4c00a30db7ec8bfb32f61eda
Ezequiel Farioli <efarioli@gmail.com>
2018-02-23 12:41:40 +0000

    define init function

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var budgetController = (function() {
+var budgetController = (function () {
     //some code
 
 })();
 
-var UIController = (function(){
+var UIController = (function () {
 
     var DOMstrings = {
         inputType: '.add__type',
@@ -13,28 +13,39 @@ var UIController = (function(){
     };
 
     return {
-        getInpunt: function() {
-            return{
-            type: document.querySelector(DOMstrings.inputType).value,// will be either inc or exp
-            description: document.querySelector(DOMstrings.inputDescription).value,
-            value: document.querySelector(DOMstrings.inputValue).value
-        };
-    },
+        getInpunt: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
 
-    getDOMStrings: function() {
-        return DOMstrings;
-    }
-   
+        getDOMStrings: function () {
+            return DOMstrings;
+        }
+
     };
 
 })();
 
-var controller = (function(budgetCtrl, UICtrl) {
+var controller = (function (budgetCtrl, UICtrl) {
     //controller is the only one module that can interact with the others modules
 
-    var DOM = UICtrl.getDOMStrings();
+    var setupEventListeners = function () {
 
-    var ctrlAddItem =function() {
+        var DOM = UICtrl.getDOMStrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+    var ctrlAddItem = function () {
         // 1. Get the field input data
 
         var input = UICtrl.getInpunt();
@@ -48,13 +59,14 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         // 5. Display the budget on the UI
     };
-    
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
-    document.addEventListener('keypress', function(event) {
-        if (event.keyCode === 13 || event.which === 13){
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log('aplicatipn started');
+            setupEventListeners();
         }
-    });
+    };
 
 })(budgetController, UIController);
+
+controller.init();
